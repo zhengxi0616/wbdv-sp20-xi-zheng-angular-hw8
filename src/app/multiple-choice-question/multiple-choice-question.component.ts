75e13ce072066255734e5c20e7eb35f73e81b15e
wuyushuwys <wuyushuwys@outlook.com>
2020-04-08 19:56:27 -0400

    Assignment8 make modify multiple choice questions

--- src/app/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/multiple-choice-question/multiple-choice-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-multiple-choice-question',
@@ -9,6 +9,9 @@ export class MultipleChoiceQuestionComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  question
+  answer = null
   ngOnInit(): void {
   }
 
